ecf68dce010daec3e17bea8c60f210e0a747341b
Renato Filho <renatosousafilho@gmail.com>
2016-09-19 16:13:45 -0300

    criando .gemspec

--- lib/moip_sandbox.rb
+++ lib/moip_sandbox.rb
@@ -4,10 +4,10 @@ require 'rest_client'
 require "base64"
 require 'nokogiri'
 
-require_relative './moip-sandbox/request'
-require_relative './moip-sandbox/account_api'
-require_relative './moip-sandbox/parser/account_parser'
-require_relative './moip-sandbox/resource/account'
+require 'moip-sandbox/request'
+require 'moip-sandbox/account_api'
+require 'moip-sandbox/parser/account_parser'
+require 'moip-sandbox/resource/account'
 
 module Moip
 	class << self
