5676a48a4782f214e58b21b6db62ea4ec769a746
Renato Filho <renatosousafilho@gmail.com>
2016-09-19 23:52:30 -0300

    Refatorando AccountApi

--- lib/moip-sandbox/account_api.rb
+++ lib/moip-sandbox/account_api.rb
@@ -1,7 +1,17 @@
 module Moip
 	class AccountAPI
-		def self.find_account(email)
-			@response = Moip::Request.build_request(:get, "https://desenvolvedor.moip.com.br/sandbox/ws/alpha", "/VerificarConta/#{email}")
+		attr_reader :client
+
+		def initialize(client)
+			@client = client
+		end
+
+		def base_path
+			"/ws/alpha/VerificarConta/"
+		end
+
+		def find(email)
+			@response = client.get("#{base_path}/#{email}")
 			@doc = Nokogiri::XML.parse(@response.to_s)
 			@account = Moip::Parser::AccountParser.parse(@doc)
 			@account
