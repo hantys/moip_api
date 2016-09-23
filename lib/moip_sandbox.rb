7568b6bf77f8e698d4863b34fa403ca02b421b5d
Renato Filho <renatosousafilho@gmail.com>
2016-09-23 14:11:41 -0300

    Removin parser from requirements

--- lib/moip_sandbox.rb
+++ lib/moip_sandbox.rb
@@ -16,7 +16,6 @@ require 'moip-sandbox/account_api'
 require 'moip-sandbox/auth_token_api'
 require 'moip-sandbox/auth/basic'
 require 'moip-sandbox/auth/oauth'
-require 'moip-sandbox/parser/account_parser'
 require 'moip-sandbox/resource/account'
 require 'moip-sandbox/resource/user'
 
