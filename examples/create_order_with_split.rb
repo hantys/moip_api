4707b491c82b6444371721ea5b060b5ec8e5ad24
pedro <pedro.fausto@hotmail.com>
2016-09-28 17:10:33 -0300

    fix bug order

--- examples/create_order_with_split.rb
+++ examples/create_order_with_split.rb
@@ -10,43 +10,13 @@ require_relative './configuration'
 
 @address = Moip::Resource::Address.new(city: "São Paulo", complement: "8", street: "Avenida Faria Lima", streetNumber: "2927", zipCode: "0123400000", state: "SP", type: "SHIPPING", country: "BRA")
 
-@customer = Moip::Resource::Customer.new(id: "fewfe", ownId: 'fefe', fullname: 'jose atonio', email: 'teste@teste.com', taxDocument: {type: 'CPF', number: '037.852.496-83'}, phone: {countryCode: '55', areaCode: '86', number: '99999-9999'}, shippingAddress: @address)
+@customer = Moip::Resource::Customer.new(ownId: 'fefe', fullname: 'jose atonio', email: 'teste@teste.com', taxDocument: {type: 'CPF', number: '037.852.496-83'}, phone: {countryCode: '55', areaCode: '86', number: '99999-9999'}, shippingAddress: @address)
 
 
-@order = Moip::Resource::Order.new(own_id: "0001", amount: @amount, receivers: [@receiver], items: [@item], customer: @customer)
+@order = Moip::Resource::Order.new(ownId: "0001", amount: @amount, items: [@item], customer: @customer)
 
 @api = Moip::Api.new
 
-@body ={
-  "ownId": "seu_identificador_proprio",
-  "amount": {
-    "currency": "BRL",
-    "subtotals": {
-      "shipping": 1000
-    }
-  },
-  "items": [
-    {
-      "product": "Descrição do pedido",
-      "quantity": 1,
-      "detail": "Mais info...",
-      "price": 1000
-    }
-  ],
-  "customer": {
-    "ownId": "cliente_xyz",
-    "fullname": "João Silva",
-    "email": "joaosilva@email.com"
-  }
-}
-
 @response = @api.order.create(@order)
-# @headers = {:"Content-Type"=>"application/json", :Authorization=>"Basic WUpORzJKSjdFRjlEMDRET01UR0U4VDZaQzJUQ1pBQ0Q6TDRCSDY3T0VPUFg4TDhLS0g5SFRUU1dNQ0ZaSDVIMkJZUzE4Rk9VSw=="}
-# @response = @api.order.client.class.post("/orders", body: @body.to_json, headers: @headers)
-
-# @response = @api.account.client.class.post "/orders", headers: @headers,  body: @body.to_json
-
-# @api.order.client.uri = "http://sandbox.moip.com.br/v2"
-# @reponse = @api.order.client.post("/orders", @body.to_json)
 
 binding.pry
--- lib/moip_api/resource/order.rb
+++ lib/moip_api/resource/order.rb
@@ -4,10 +4,10 @@ module Moip
     class Order
       include ActiveData::Model
 
-      attribute :own_id, type: String
+      attribute :ownId, type: String
       attribute :customer, type: Object # Pode ser o id  de um cliente ja existente ou um novo cliente
       attribute :amount, type: Object
-      attribute :receivers, type: Array
+      attribute :receivers, type: Array, default: []
       attribute :items, type: Array
 
     end
