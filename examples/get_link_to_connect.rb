14fe07e8489591afc6ec8e6e3aee77890b8e7f4c
Renato Filho <renatosousafilho@gmail.com>
2016-09-30 10:49:02 -0300

    example to generate link

--- examples/get_link_to_connect.rb
+++ examples/get_link_to_connect.rb
@@ -2,11 +2,9 @@ require 'moip_api'
 require 'pry'
 require_relative './configuration'
 
-@order_id = "ORD-X7FR9T67K50C"
-
 @api = Moip::Api.new
 
-@response = @api.order.show(@order_id)
+@response = @api.connect.get_permission_uri
 
+p @response
 
-binding.pry
