74e4b8ea7fc8769c0cc594ee4e1af38822efaf16
renatosousafilho <renatosousafilho@gmail.com>
2016-11-30 17:10:14 -0300

    add callback url in config

--- lib/moip_api/config.rb
+++ lib/moip_api/config.rb
@@ -1,6 +1,6 @@
 module Moip
 	class Config
-		attr_accessor :api_token, :api_secret, :app_id, :app_access_token, :app_secret, :app_redirect_uri, :environment
+		attr_accessor :api_token, :api_secret, :app_id, :app_access_token, :app_secret, :app_redirect_uri, :environment, :callback_url
 	end
 end
 
