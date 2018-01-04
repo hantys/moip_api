4686e6dcf696985e584bdcc0edd704852c471a14
juniornelson123 <junior123nelson@gmail.com>
2018-01-03 21:14:28 -0300

    fazendo testa na api v2

--- lib/moip_api/client.rb
+++ lib/moip_api/client.rb
@@ -72,7 +72,7 @@ module Moip
 		def get_base_uri
 			if @version == :v2
 				if production?
-					(host== :connect) ? "https://connect.moip.com.br" : "https://api.moip.com.br"
+					(host== :connect) ? "https://connect.moip.com.br" : "https://api.moip.com.br/v2"
 				else
 					(host== :connect) ? "https://connect-sandbox.moip.com.br" : "https://sandbox.moip.com.br/v2"
 				end
